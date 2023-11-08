5f3cd09faf552b6d075c88cccccb9c9ca9d80e8d
rezaafaisal <rezha.faizal567@gmail.com>
2023-11-08 12:02:41 +0800

    update

--- dashboard/pages/4_About.py
+++ dashboard/pages/4_About.py
@@ -4,7 +4,7 @@ st.write("<div style='text-align: center;font-size: 25px;font-weight: bold;margi
 
 col_1, col_2 = st.columns([1,2])
 with col_1:
-    st.write("<div style=''><img style='border-radius:50%' src='https://raw.githubusercontent.com/rezaafaisal/source/main/Screenshot%202023-06-08%20at%2022.29.32.png' width='200px' height='200px' /></div>", unsafe_allow_html=True)
+    st.write("<div style=''><img style='border-radius:50%' src='https://raw.githubusercontent.com/rezaafaisal/source/images/photo/cat.jpeg' width='200px' height='200px' /></div>", unsafe_allow_html=True)
 
 with col_2:
     st.text("Nama : Reza Faisal")
@@ -13,10 +13,10 @@ with col_2:
     st.write("""
              <div>
                 <a href='https://instagram.com/rezaa.faisal' style='margin-right:5px;' target='_blank'>
-                    <img src='https://raw.githubusercontent.com/rezaafaisal/source/main/instagram.png' height='20px' width='20px'>
+                    <img src='https://raw.githubusercontent.com/rezaafaisal/source/images/icon/instagram.png' height='20px' width='20px'>
                 </a>
                 <a href='https://linkedin.com/in/rezafaisal' target='_blank'>
-                    <img src='https://raw.githubusercontent.com/rezaafaisal/source/main/linkedin.png' height='20px' width='20px'>
+                    <img src='https://raw.githubusercontent.com/rezaafaisal/source/images/icon/linkedin.png' height='20px' width='20px'>
                 </a>
              </div>
              
